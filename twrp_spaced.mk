e5c7ee209fdfa6c5f83ab8ecfb7cef4c97424595
D Ξ Ξ Z N U T Z <daizeuzdred@gmail.com>
2026-06-28 14:52:40 +0800

    Update twrp_spaced.mk

--- twrp_spaced.mk
+++ twrp_spaced.mk
@@ -22,7 +22,7 @@ PRODUCT_RELEASE_NAME := spaced
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from spaced device
 $(call inherit-product, device/realme/spaced/device.mk)
